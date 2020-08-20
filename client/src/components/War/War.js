6f3d3e71cb46cc1a7819a27a91de4faf5d3cbde2
Jackie Chiem <32667298+chiemjackie@users.noreply.github.com>
2020-08-20 01:30:34 -0400

    Implemented basic card component (to be replcaed later)

--- client/src/components/War/War.js
+++ client/src/components/War/War.js
@@ -1,27 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+import Card from "react-playing-card";
 
-import { CardSingle } from "../CardGraphics/Cards";
+// import { CardSingle } from "../CardGraphics/Cards";
 
 const War = () => {
-  return (
-    <CardSingle
-      id={`S_A`}
-      card={{
-        suit: `S`,
-        rank: `A`,
-        backColor: `#1A1919`,
-        // color: item.suit === "D" || item.suit === "H" ? `#D33E43` : `#1A1919`,
-      }}
-      // status={?}
-    />
-    // substitute for item here?
-  );
+  return <StyledCard rank="J" suit="C" />;
 };
 
-const StyledCard = styled(CardSingle)`
-  padding: 50px;
-  background-color: red;
+const StyledCard = styled(Card)`
+  display: none;
 `;
 
 export default War;
