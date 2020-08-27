38f77af25fee171572183d85e6417225d6b2bedd
Jackie Chiem <32667298+chiemjackie@users.noreply.github.com>
2020-08-26 21:20:58 -0400

    Connected MongoDB

--- server/file.js
+++ server/file.js
@@ -1,4 +1,25 @@
 const { MongoClient } = require("mongodb");
 
-require("dotenv".config();
-const {MONGO_URI} = process.env;
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const dbFunction = async (dbName) => {
+  const client = await MongoClient(MONGO_URI, options);
+
+  await client.connect();
+
+  const db = client.db(dbName);
+  console.log("connected!");
+
+  await db.collection("test").insertOne({ name: "test test" });
+
+  client.close();
+  console.log("disconnected!");
+};
+
+dbFunction("carden");
